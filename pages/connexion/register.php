9e4ab4ea7a48c4dc8b01847057f26dfb823683d6
Elforcado95 <mbogneelvis@yahoo.fr>
2018-07-04 19:26:42 +0100

    debut du projet

---
+++ pages/connexion/register.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html5>
+<html>
+    <head>
+
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Mbogne Elvis </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" media="screen" href="/styles/style.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/styles/bootstrap.css" id="bootstrap-css"/>
+        <link href="/styles/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="screen" />
+        <script src="/scripts/bootstrap.min.js"></script>
+        <script type="text/javascript" language="javascript" src="/scripts/jquery-1.11.1.js"></script>
+        <script type="text/javascript" language="javascript" src="/scripts/jquery.validate.js"></script>
+        <!--fornction de control du formulaire-->
+
+    </head>
+    <body>
+    <div class="tout">
+        <!--Header-->
+            <div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-6 logo">
+                            <img class="img-fluid" alt="Responsive image" src ="/images/elvis-logo.png" alt = "Mbogne elvis">
+                        </div>
+                        <div class="col-12 col-md-2 anim1" >
+                            <img class="img-fluid" alt="Responsive image" src="/images/1.gif" width="150" height="80">
+                        </div>
+                        <div class="col-12 col-md-2 anim2">
+                            <img class="img-fluid" alt="Responsive image" src="/images/2.gif" width="150" height="80">
+                        </div>
+                        <div class="col-12 col-md-2 anim3">
+                            <img class="img-fluid" alt="Responsive image" src="/images/3.gif" width="145" height="80">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <nav class="navbar navbar-default navbar-inverse nav1" role="navigation">
+                <div class="container-fluid nav1">
+                <!-- Brand and toggle get grouped for better mobile display -->
+              
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle coulmenu" data-toggle="dropdown">Accueil <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#"> Presentation </a></li>
+                                    <li><a href="#"> Mon CV </a></li>
+                                    <li><a href="#">Mes Actualités </a></li>
+                                    <li class="divider"></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle coulmenu" data-toggle="dropdown"> Formation <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#"> 3ds Max </a></li>
+                                    <li><a href="#"> Motion Design </a></li>
+                                    <li><a href="#"> Animation </a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Compositing</a></li>
+                                    <li class="divider"></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle coulmenu" data-toggle="dropdown">Traveaux <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#"> Images, dessins </a></li>
+                                    <li><a href="#"> Videos </a></li>
+                                    <li><a href="#"> Sites Web </a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">prestations </a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Search</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"> Forum </a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle coulmenu" data-toggle="dropdown"> Contact <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#"> Me contacter </a></li>
+                                    <li><a href="#"> Mes partenaires </a></li>
+                                    <li class="divider"></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<i class="fa fa-user mess2"></i></a>
+                                <ul id="login-dp" class="dropdown-menu">
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                Se connecter avec
+                                                <div class="social-buttons">
+                                                    <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                                                    <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                                                </div>
+                                                ou
+                                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                                    <div class="form-group">
+                                                        <label class="sr-only" for="exampleInputEmail2">Adresse Mail</label>
+                                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Adresse Mail" required>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="sr-only" for="exampleInputPassword2">Mot de Pass</label>
+                                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Mot de Pass" required>
+                                                        <div class="help-block text-right"><a href="">Mot de pass oublier ?</a></div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button type="submit" class="btn btn-primary btn-block">Se Connecter</button>
+                                                    </div>
+                                                    <div class="checkbox">
+                                                        <label>
+                                                        <input type="checkbox"> Rester connecter !
+                                                        </label>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="bottom text-center">
+                                                Nouveau ? <a href="#"><b>Créer un compte</b></a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>     
+        <!--Fin Header-->
+
+        <!--debut du contenu du body-->
+        <div class="container-fluid">
+            <form id="signupForm1" method="post" class="form1" action="">
+                <div class="row">
+                    <h4>Créer Un compte</h4>
+                    Se connecter avec
+                    <div class="social-buttons">
+                        <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                        <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                        <a href="#" class="btn btn-gplus"><i class="fa fa-google-plus"></i> google-Plus</a>
+                    </div>
+                    <br />
+                    <div class="input-group input-group-icon champs">
+                        <div class="input-icon">
+                            <i class="fa fa-user mess"></i>
+                            <input type="text" placeholder="Pseudo | Nom" name="nom" id="nom"/>
+                        </div>
+                    </div>
+                    <div class="input-group input-group-icon champs">
+                        <div class="input-icon">
+                            <i class="fa fa-envelope mess"></i>
+                            <input type="email" placeholder="Adresse Mail" name="mail" id="mail"/>
+                        </div>
+                    </div>
+                    <div class="input-group input-group-icon champs">
+                        <div class="input-icon">
+                            <i class="fa fa-key mess1"></i>
+                            <input type="password" placeholder="Mot de pass" name="mdp" id="mdp"/>
+                        </div>
+                    </div>
+                    <div class="input-group input-group-icon champs">
+                        <div class="input-icon">
+                            <i class="fa fa-key mess1"></i>
+                            <input type="password" placeholder="Confimé le mot de pass" name="cmdp" id="cmdp"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-half">
+                        <div class="input-group input-group-icon champs">
+                            <h4>Date de naissance</h4>
+                            <i class="fa fa-calendar caland"></i>
+                            <input type="date" name="bday" id="bday">
+                        </div>
+                    </div>
+                    <h4>Genre</h4>
+                    <div class="col-half champs">
+                        <div class="input-group">
+                            <i class="fa fa-male caland"></i>
+                            <input type="radio" name="gender" value="male" id="gender-male"/>
+                            <label for="gender-male">Homme </label>
+                            <i class="fa fa-female caland"></i>
+                            <input type="radio" name="gender" value="female" id="gender-female"/>
+                            <label for="gender-female"> Femme</label>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row champs">
+                    <h4>Condition d'utilisation</h4>
+                    <div class="input-group">
+                        <input type="checkbox" name="terms" id="terms"/>
+                        <label for="terms">
+                            J'accepte les termes et conditions pour m'inscrire à ce service, 
+                            et confirme par la présente que j'ai lu la politique de confidentialité.
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-4">
+                        <button type="submit" class="btn btn-primary" name="signup1" value="Sign up">Créer un compte</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!--Fin contenu-->
+
+        <!--Debut footer-->
+        <div class="container-fluid foot">
+            <div class="row">
+                <div class="col-12 col-md-2 Fancre">
+                    <span class="footinfo"> Téléphone: </span>
+                    <span > 695260475 </span><br />
+                    <span class="footinfo"> mail: </span>
+                    <span> mbogneelvis@yahoo.fr </span>
+                </div>
+                <div class="col-12 col-md-4 Fdroits" >
+                    <i><span class="footinfodroit"> Copyright@ Mbogne Elvis 2018 </span></i>
+                </div>
+                <div class="col-12 col-md-6 Freseaux">
+                    <span class="footinfo"> Suivez moi Sur:</span><br /><br />
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 col-md-2 y" >
+                                <span> youtube </span>
+                                <a href="#"><i class="fa fa-youtube-square fa-2x messy"></i></a> 
+                            </div>
+                            <div class="col-12 col-md-2 w">
+                                <span> Watsapp </span>
+                                <a href="#"><i class="fa fa-whatsapp fa-2x messw"></i></a> 
+                            </div>
+                            <div class="col-12 col-md-2 l">
+                                <span> LinkedIn </span>
+                                <a href="#"><i class="fa fa-linkedin-square fa-2x messl"></i></a>
+                            </div>
+                            <div class="col-12 col-md-2 g">
+                                <span> Google+ </span>
+                                <a href="#"><i class="fa fa-google-plus-square fa-2x messg"></i></a>
+                            </div>
+                            <div class="col-12 col-md-2 f" >
+                                <span> Facebook </span>
+                                <a href="#"><i class="fa fa-facebook-square fa-2x messf"></i></a> 
+                            </div>
+                            <div class="col-12 col-md-2 t">
+                                <span> Twitter </span>
+                                <a href="#"><i class="fa fa-twitter-square fa-2x messt"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Fin footer-->
+    </div>
+
+    <!-- Validation du formulaire -->
+    <script type="text/javascript">
+		$.validator.setDefaults( {
+			submitHandler: function () {
+				alert( "Merci de nous faire confiance !" );
+			}
+		} );
+
+		$( document ).ready( function () {
+			$( "#signupForm1" ).validate( {
+				rules: {
+					nom: {
+						required: true,
+                        minlength: 5
+					},
+					mdp: {
+						required: true,
+						minlength: 8
+					},
+					cmdp: {
+						required: true,
+						minlength: 8,
+						equalTo: "#mdp"
+					},
+					mail: {
+						required: true,
+                        email: true
+					},
+                    terms: "required",
+                    bday: "required"
+				},
+				messages: {
+					nom: {
+						required: "Veuillez entrer un nom",
+						minlength: "Votre nom doit contenir au moins 5 caractères"
+					},
+					mdp: {
+						required: "Veuillez un mot de pass",
+						minlength: "Votre pass doit contenir au moins 8 caractères"
+					},
+					cmdp: {
+						required: "Veuillez un mot de pass",
+						minlength: "Votre pass doit contenir au moins 8 caractères",
+						equalTo: "Mot de pass non similaire"
+                    },
+                    required: "Veuillez entrer un email",
+					mail: "Entrer un email Valide",
+					terms: "Vous devez Accepter les termes d'utilisation"
+				},
+				errorElement: "em",
+				errorPlacement: function ( error, element ) {
+					// Add the `help-block` class to the error element
+					error.addClass( "help-block" );
+
+					// Add `has-feedback` class to the parent div.form-group
+					// in order to add icons to inputs
+					element.parents( ".champs" ).addClass( "has-feedback" );
+
+					if ( element.prop( "type" ) === "checkbox" ) {
+						error.insertAfter( element.parent( "label" ) );
+					} else {
+						error.insertAfter( element );
+					}
+
+					// Add the span element, if doesn't exists, and apply the icon classes to it.
+					if ( !element.next( "i" )[ 0 ] ) {
+						$( "<i class='fa fa-times Novalid form-control-feedback'></>" ).insertAfter( element );
+					}
+				},
+				success: function ( label, element ) {
+					// Add the span element, if doesn't exists, and apply the icon classes to it.
+					if ( !$( element ).next( "i" )[ 0 ] ) {
+						$( "<i class='fa fa-check form-control-feedback'></i>" ).insertAfter( $( element ) );
+					}
+				},
+				highlight: function ( element, errorClass, validClass ) {
+					$( element ).parents( ".champs" ).addClass( "has-error" ).removeClass( "has-success" );
+					$( element ).next( "i" ).addClass( "fa-times Novalid" ).removeClass( "fa-check" );
+				},
+				unhighlight: function ( element, errorClass, validClass ) {
+					$( element ).parents( ".champs" ).addClass( "has-success" ).removeClass( "has-error" );
+					$( element ).next( "i" ).addClass( "fa-check" ).removeClass( "fa-times" );
+				}
+			} );
+		} );
+	</script>
+
+    </body>
+</html>
